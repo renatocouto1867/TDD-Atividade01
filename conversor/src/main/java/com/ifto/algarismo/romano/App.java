1ea4acb7283fc7718c3562fdc3727a6a22dc140d
renatocouto1867 <renato.couto@estudante.ifto.edu.br>
2023-11-05 19:06:24 -0300

    FINAL

--- conversor/src/main/java/com/ifto/algarismo/romano/App.java
+++ conversor/src/main/java/com/ifto/algarismo/romano/App.java
@@ -3,8 +3,7 @@ package com.ifto.algarismo.romano;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         System.out.print("O Sistema converte numeros Arabicos em Romanos\n" +
                 "o sistema tambem converte Romanos em Arabicos\nDigite um Valor: ");
         String numero = (new Scanner(System.in)).nextLine();
@@ -12,7 +11,6 @@ public class App {
         ConversorRomano conversor = new ConversorRomano();
 
         System.out.println(conversor.converte(numero));
-
 
     }
 }
